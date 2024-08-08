be24be46d172b091c419a36a7ce0348e86c3f7fb
lemurlenok <lemurlenok@gmail.com>
2024-08-08 12:54:08 +0300

    l-1

---
+++ src/app/users/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'UsersLayout metadata'
+}
+
+type Props = { children: React.ReactNode }
+
+const UsersLayout = ({children}: Props) => {
+    return (
+        <>
+            {children}
+        </>
+    );
+};
+
+export default UsersLayout;
